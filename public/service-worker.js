64ba63f000ebc82db35fabfa3717782684492246
Priyanshu Bhardwaj <iampriyanshubhardwaj@gmail.com>
2026-01-29 16:38:08 +0530

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,46 @@
+// service-worker.js
+const CACHE_NAME = 'shredit-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo144.png',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  // Clean up old caches
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
